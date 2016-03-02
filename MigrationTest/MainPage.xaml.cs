0811a9143e556ed4db0a12e0203e2c52af46f306
Geoffrey Morgan <geoffrey.morgan@live.com>
2016-03-02 10:31:32 -0700

    Initial Checkin

---
+++ MigrationTest/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Graphics.Canvas;
+using System;
+using Windows.UI.Xaml.Controls;
+
+namespace MigrationTest
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Load();
+        }
+
+        public async void Load()
+        {
+            await CanvasBitmap.LoadAsync(this.BlurredImage, new Uri("http://logok.org/wp-content/uploads/2014/09/Bing-logo-880x633.png"));
+        }
+    }
+}
---
+++ MigrationTest/Models/MigrationTestDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Entity;
+using MigrationTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationTest.Models
+{
+    public class MigrationTestDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename=migrationTest.db");
+        }
+    }
+}
